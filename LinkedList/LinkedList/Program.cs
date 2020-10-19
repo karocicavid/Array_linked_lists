83977b84d8e0fadfaf9538d6d11172c4f329d268
karocicavid <69958924+karocicavid@users.noreply.github.com>
2020-10-19 18:24:56 +0300

    Update Program.cs

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -38,51 +38,66 @@ namespace cavka
     {
         static void Main()
         {
-            LinkedList<Student> studentlinked = new LinkedList<Student>();
-            LinkedList<Aspirant> aspirantlinked = new LinkedList<Aspirant>();
-            Console.Write("If you wanna register as student press - 1 , as aspirant press - 2 : ");
-            int press12 = TryPress12();
-            switch (press12)
+            bool check = true;
+            do
             {
-               case 1:
-                   {
-                       Console.Write("Input name of student : "); string name = Alphabet();
-                       Console.Write("Input surname of student : "); string surname = Alphabet();
-                       Console.Write("Input course of student : "); int course = TryPress();
-                       Console.Write("Input codebook of student : "); int codebook = TryPress();
-                       Student student = new Student(name, surname, course, codebook);
-                        studentlinked.AddFirst(student);
-                       break;
-                   }
-               case 2:
-                   {
-                       Console.Write("Input name of aspirant : "); string name = Alphabet();
-                       Console.Write("Input surname of aspirant : "); string surname = Alphabet();
-                       Console.Write("Input course of aspirant : "); int course = TryPress();
-                       Console.Write("Input codebook of aspirant : "); int codebook = TryPress();
-                       Console.Write("Input disertation of aspirant : "); string disertation = Console.ReadLine();
-                       Aspirant aspirant = new Aspirant(name, surname, course, codebook, disertation);
-                        aspirantlinked.AddFirst(aspirant);
-                       break;
-                   }
-            }
-            Console.Write("\nFor continue press - 1 , for see all users registered list and exit press - 2 : ");
-            int menu = TryPress12();
-            switch (menu)
-            {
-                case 1:
-                    {
-                        Main();
-                        break;
-                    }
-                case 2:
-                    { 
-                        break;
-                    }
-            }
-            Console.WriteLine("\nName of aspirant - " + aspirantlinked.First.Value.name + ",surname - " + aspirantlinked.First.Value.surname + ",course - " + aspirantlinked.First.Value.course +
-                ",codebook - " + aspirantlinked.First.Value.name + ",disertation - " + aspirantlinked.First.Value.disertation);
-            
+                LinkedList<Student> studentlinked = new LinkedList<Student>();
+                LinkedList<Aspirant> aspirantlinked = new LinkedList<Aspirant>();
+                Console.Write("If you wanna register as student press - 1 :\nIf you wanna register as aspirant press - 2 : ");
+                int press12 = TryPress12();
+                switch (press12)
+                {
+                    case 1:
+                        {
+                            Console.Write("Input name of student : "); string name = Alphabet();
+                            Console.Write("Input surname of student : "); string surname = Alphabet();
+                            Console.Write("Input course of student : "); int course = TryPress();
+                            Console.Write("Input codebook of student : "); int codebook = TryPress();
+                            Student student = new Student(name, surname, course, codebook);
+                            studentlinked.AddFirst(student);
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.Write("Input name of aspirant : "); string name = Alphabet();
+                            Console.Write("Input surname of aspirant : "); string surname = Alphabet();
+                            Console.Write("Input course of aspirant : "); int course = TryPress();
+                            Console.Write("Input codebook of aspirant : "); int codebook = TryPress();
+                            Console.Write("Input disertation of aspirant : "); string disertation = Console.ReadLine();
+                            Aspirant aspirant = new Aspirant(name, surname, course, codebook, disertation);
+                            aspirantlinked.AddFirst(aspirant);
+                            break;
+                        }
+                }
+                Console.Write("\nFor continue press - 1 \nFor see all users registered list and exit press - 2 : ");
+                int menu = TryPress12();
+                switch (menu)
+                {
+                    case 1:
+                        {
+
+                            break;
+                        }
+                    case 2:
+                        {
+                            check = false;
+                            break;
+                        }
+                }
+                foreach(Student st in studentlinked)
+                {
+                    Console.WriteLine("\nName of aspirant - " + st.name + ",surname - " + st.surname + ",course - " + st.course +
+                 ",codebook - " + st.name );
+                }
+                foreach(Aspirant asp in aspirantlinked)
+                {
+                    Console.WriteLine("\nName of aspirant - " + asp.name + ",surname - " + asp.surname + ",course - " + asp.course +
+                    ",codebook - " + asp.name + ",disertation - " + asp.disertation);
+                }
+
+            } while (check == true);
+            Console.Read();
+          
         }
         public static int TryPress12()
         {
